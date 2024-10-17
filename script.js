f90e4eba52c412a141fa8bf0bb4aaec1a67f8821
Makvi-droid <pasbenauro05@gmail.com>
2024-10-17 21:24:04 +0800

    finished simple calculator

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ $(document).ready(function(){
     });
 });
 
+//calculator
+$(document).ready(function(){
+    $('.addBtn').click(function(){
+        let sum = parseFloat($('#num1').val()) + parseFloat($('#num2').val());
+        return $('.answer').text(`The sum is ${sum}`);
+    });
+
+    $('.subtractBtn').click(function(){
+        let difference = parseFloat($('#num1').val()) - parseFloat($('#num2').val());
+        return $('.answer').text(`The difference is ${difference}`);
+    });
+
+    $('.divideBtn').click(function(){
+        let quotient = parseFloat($('#num1').val()) / parseFloat($('#num2').val());
+        return $('.answer').text(`The quotient is ${quotient}`);
+    });
+
+    $('.multiplyBtn').click(function(){
+        let product = parseFloat($('#num1').val()) * parseFloat($('#num2').val());
+        return $('.answer').text(`The sum is ${product}`);
+    });
+});
